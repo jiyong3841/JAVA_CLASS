5aa5ae5250a5aac7fff06b380d71716a4647da28
Admin <Admin@F-4>
2025-02-04 16:49:10 +0900

    DAY 1

---
+++ java/src/day1/_02_EscapeSequnce.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class _02_EscapeSequnce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Write\n\nonce");//줄바꿈
+		System.out.println("사과\t배\t오렌지");//탭
+		System.out.println("apple\tpear\torange");
+		System.out.println("우리집 강쥐는 \"멍멍\" 짖어요");//큰따옴표 문자열처리
+		//한줄 주석
+		/*
+		 * 여
+		 * 러
+		 * 줄
+		 * 주
+		 * 석
+		 * */
+	}
+
+}
---
+++ java/src/day1/_03_print.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class _03_print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("apple");
+		System.out.println("bannana");
+		System.out.println("orange");
+		// 문자열을 출력하므로 큰따옴표 사용
+		
+		System.out.println("");
+		System.out.println(5);
+		System.out.println(3.14);
+		System.out.println('A');
+	}
+
+}
